5866d92b6dc419ee9a4098ba9db0dfa2c8ff4d35
Raymond Cavalcante <cavalcante.raymond@gmail.com>
2018-04-11 09:43:03 -0400

    Better document packrat library for ERRBS
    * We need a full complement of CRAN, Bioc, Git, and custom packages.
    * methylSig is currently the v0.5.0-beta branch 
    * We need to install a modified version of GenomeInfoDb (modified from the RELEASE_3_6 branch of Bioc) to correctly fetch Seqinfo from URLs on flux worker nodes.

--- packrat_errbs.R
+++ packrat_errbs.R
@@ -1,8 +1,40 @@
-packrat::init('/ccmb/BioinfCore/ActiveProjects/EpiCore/bin/packrat_errbs')
+packrat_dir = '/nfs/med-bfx-activeprojects/EpiCore/bin/packrat_errbs'
 
-install.packages(c('devtools', 'optparse', 'gplots', 'RColorBrewer'), repos='https://cran.rstudio.com')
+if(!dir.exists(packrat_dir)) {
+    dir.create(packrat_dir, mode = '775')
+}
+
+packrat::init(packrat_dir, enter = TRUE)
+
+cran_libs = c(
+    'readr',
+    'ggplot2',
+    'reshape2',
+    'devtools',
+    'optparse',
+    'gplots',
+    'RColorBrewer',
+    'openxlsx',
+    'rmarkdown')
+
+install.packages(cran_libs, repos='https://cran.rstudio.com')
+
+bioc_libs = c(
+    'annotatr',
+    'chipenrich',
+    'bsseq',
+    'limma',
+    'org.Hs.eg.db',
+    'org.Mm.eg.db',
+    'DSS')
 
 source("https://bioconductor.org/biocLite.R")
-biocLite(c('annotatr', 'rtracklayer'))
+biocLite(bioc_libs, ask = FALSE)
 
-devtools::install_github('sartorlab/methylSig@v0.5.0-beta')
+git_libs = c(
+    'sartorlab/methylSig@v0.5.0-beta'
+)
+
+devtools::install_github(git_libs)
+
+install.packages('/nfs/med-bfx-activeprojects/EpiCore/rcavalca/repos/GenomeInfoDb_1.14.1.tar.gz', type = 'source')
